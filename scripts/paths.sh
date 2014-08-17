2704a0af0d7395e5b1f572c1af1a7e88e1847d31
Anirudh Kaushik <amkaushi@uwaterloo.ca>
2014-08-17 14:13:14 -0400

    clean

--- scripts/paths.sh
+++
@@ -1,4 +0,0 @@
-export LLVM_SRC_DIR=/home/$HOME/workspace/code/systemc-clang/llvm
-export LLVM_BUILD_DIR=/home/$HOME/workspace/code/systemc-clang/build
-export CLANG_SRC_DIR=/home/$HOME/workspace/code/systemc-clang/llvm/tools/clang
-export CLANG_BUILD_DIR=/home/$HOME/workspace/code/systemc-clang/build/tools/clang
