3302c59f92bcf902b0135d62a30e5d4163d91568
Twiga <twiga@caesr-x240.uwaterloo.ca>
2018-12-21 11:16:48 -0500

    clean up code.

--- src/InterfaceDecl.cpp
+++ src/InterfaceDecl.cpp
@@ -4,50 +4,41 @@
 using namespace scpar;
 using namespace std;
 
-InterfaceDecl::~InterfaceDecl()
-{
-
-  if (_templateType != NULL) {
+InterfaceDecl::~InterfaceDecl() {
+  if (_templateType != nullptr) {
     delete _templateType;
   }
 }
 
-InterfaceDecl::InterfaceDecl():
-_name("NONE"), _templateType(NULL)
-{
-
+InterfaceDecl::InterfaceDecl() :
+  _name{"NONE"},
+  _templateType{nullptr} {
 }
 
-InterfaceDecl::InterfaceDecl(const string & name, FindTemplateTypes * tt)
-:_name(name), _templateType(tt)
-{
-
+InterfaceDecl::InterfaceDecl( const string &name, FindTemplateTypes * tt ) :
+  _name{name},
+  _templateType{tt} {
 }
 
-InterfaceDecl::InterfaceDecl(const InterfaceDecl & from)
-{
+InterfaceDecl::InterfaceDecl( const InterfaceDecl &from ) {
   _name = from._name;
   // This is necessary to allow FindInterfaces to go out of scope.
-  _templateType = new FindTemplateTypes(*from._templateType);
+  _templateType = new FindTemplateTypes( *from._templateType );
 }
 
-void InterfaceDecl::setModuleName(const string & name)
-{
+void InterfaceDecl::setModuleName( const string &name ) {
   _name = name;
 }
 
-string InterfaceDecl::getName()
-{
+string InterfaceDecl::getName() {
   return _name;
 }
 
-FindTemplateTypes *InterfaceDecl::getTemplateType()
-{
+FindTemplateTypes *InterfaceDecl::getTemplateType() {
   return _templateType;
 }
 
-void InterfaceDecl::dump(raw_ostream & os, int tabn)
-{
+void InterfaceDecl::dump(raw_ostream & os, int tabn) {
   for (int i = 0; i < tabn; i++) {
     os << " ";
   }
