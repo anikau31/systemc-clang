9dd5afeb4da1b211cb3fd4bd5d7aa9d4d8fcb44d
rmrf <nospam@uwaterloo.ca>
2019-07-30 21:31:07 -0400

    Start adding tests.

---
+++ tests/basic-module-thread.cpp
@@ -0,0 +1,31 @@
+#include "systemc.h"
+SC_MODULE( test ){
+    sc_in<int> in;
+    sc_out<int> out;
+    sc_signal<int> test_signal;
+    void entry_function_1() {
+        while(true) {
+            // do nothing
+        }
+    }
+
+    void entry_function_2() {
+        while(true) {
+            // do nothing
+        }
+    }
+
+    SC_CTOR( test ) {
+        SC_THREAD(entry_function_1);
+        SC_THREAD(entry_function_2);
+    }
+};
+
+int sc_main(int argc, char *argv[]) {
+    sc_signal<int> sig1;
+    test test_instance("testing");
+    test_instance.in(sig1);
+    test_instance.out(sig1);
+
+    return 0;
+}
