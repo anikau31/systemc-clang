3699710198815bc1d76a60a28aae5b00342d3a58
rmrf <nospam@uwaterloo.ca>
2019-12-09 12:03:57 -0500

    Introduce matching of user-defined types as well in test.

--- tests/t4-matchers.cpp
+++ tests/t4-matchers.cpp
@@ -21,7 +21,7 @@ using namespace sc_ast_matchers;
 
 TEST_CASE("Read SystemC model from file for testing", "[parsing]") {
   std::string code{systemc_clang::read_systemc_file(
-      systemc_clang::test_data_dir, "basic-module-method.cpp")};
+      systemc_clang::test_data_dir, "matcher-test.cpp")};
 
   ASTUnit *from_ast =
       tooling::buildASTFromCodeWithArgs(code, systemc_clang::catch_test_args)
