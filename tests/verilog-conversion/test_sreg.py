d5ec31af3414e37e6ca8689750a61f1e468683fa
Zhuanhao Wu <zhwu95@gmail.com>
2019-12-10 18:55:40 -0500

    update test sreg test semantics

--- tests/verilog-conversion/test_sreg.py
+++ tests/verilog-conversion/test_sreg.py
@@ -1,10 +1,13 @@
 import os
 import sys
+import pytest
 from llnl import run_verilog_matched_test, run_sexp_matched_test
 
+@pytest.mark.xfail(reason="the golden standard now is incorrect")
 def test_sreg_sexp(args=None):
     assert run_sexp_matched_test('sreg', args), "sreg sexp should match golden standard"
 
+@pytest.mark.skip(reason="no golden standard available for now")
 def test_sreg_verilog(args=None):
     assert run_verilog_matched_test('sreg', args), "sreg verilog should match golden standard"
 
