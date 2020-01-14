e3deb357a59ec98f1d86c0b6b2fd953d9f974eda
Zhuanhao Wu <zhwu95@gmail.com>
2020-01-13 19:11:42 -0500

    Update the example for driver

--- tests/verilog-conversion/run.py
+++ tests/verilog-conversion/run.py
@@ -9,8 +9,8 @@ def main():
     filename='~/working/systemc-clang/tests/data/verilog-conversion/llnl-examples/sreg.cpp'
     # print(' '.join(driver.systemc_clang_commandline(filename=filename)))
     # tempfolder = tempfile.mkdtemp()
-    # driver.generate_sexp(path=filename, output_folder='.', verbose=True, keep_sexp=True)
-    # driver.generate_verilog_from_sexp(path='./sreg_hdl.txt', output_folder='.', verbose=True, keep_v=True)
+    driver.generate_sexp(path=filename, output_folder='.', verbose=True, keep_sexp=True)
+    driver.generate_verilog_from_sexp(path='./sreg_hdl.txt', output_folder='.', verbose=True, keep_v=True)
 
     res = VerilogParser.diff('./sreg_hdl.txt.v', './sreg_hdl.txt.new.v')
     print('Are they same? ', res)
