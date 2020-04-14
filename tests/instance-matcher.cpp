758b426bd406764e22c600c4ba1e1438a829a319
rmrf <nospam@uwaterloo.ca>
2020-04-14 10:13:00 -0400

    Add an instance matcher test

---
+++ tests/instance-matcher.cpp
@@ -0,0 +1,37 @@
+#include "catch.hpp"
+#include "SystemCClang.h"
+#include "clang/Tooling/Tooling.h"
+
+#include "Matchers.h"
+
+// This is automatically generated from cmake.
+#include "ClangArgs.h"
+#include "Testing.h"
+
+using namespace clang::ast_matchers;
+using namespace scpar;
+using namespace sc_ast_matchers;
+
+TEST_CASE("Read SystemC model from file for testing", "[parsing]") {
+  std::string code{systemc_clang::read_systemc_file(
+      systemc_clang::test_data_dir, "matcher-test.cpp")};
+
+  ASTUnit *from_ast =
+      tooling::buildASTFromCodeWithArgs(code, systemc_clang::catch_test_args)
+          .release();
+
+  llvm::outs() << "================ TESTMATCHER =============== \n";
+  InstanceMatcher inst_matcher{};
+  MatchFinder matchRegistry{};
+  inst_matcher.registerMatchers(matchRegistry);
+  // Run all the matchers
+  matchRegistry.matchAST(from_ast->getASTContext());
+  inst_matcher.dump();
+  llvm::outs() << "================ END =============== \n";
+
+
+  SECTION("Test instance matcher", "[instance-matcher]") {
+    // The module instances have all the information.
+    REQUIRE( false );
+  }
+}
