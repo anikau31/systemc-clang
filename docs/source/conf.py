242c0451db71f36b2eb0060ad09351fdb8a7c48e
rmrf <nospam@uwaterloo.ca>
2020-06-30 01:18:12 -0400

    Allow for warnings on RTD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -70,7 +70,7 @@ html_theme = 'sphinx_rtd_theme' #'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 #html_static_path = ['_static']
-html_extra_path = ['doxydoc/html']
+#html_extra_path = ['doxydoc/html']
 
 # -- Breath and Exhale 
 
