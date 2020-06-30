0f59c36c4f382d51c393a3b290560b51909f1b38
rmrf <nospam@uwaterloo.ca>
2020-06-30 00:43:57 -0400

    Build doxygen

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,9 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import subprocess
+
+subprocess.call('doxygen Doxyfile', shell=True)
 
 # -- Project information -----------------------------------------------------
 
@@ -58,6 +61,7 @@ html_theme = 'sphinx_rtd_theme' #'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_extra_path = ['doxydoc/html']
 
 # -- Breath and Exhale 
 
