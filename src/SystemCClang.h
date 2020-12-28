286fd1a8ba321c7b78f8d39e04047e308a06d24e
rmrf <nospam@uwaterloo.ca>
2020-12-28 11:00:05 -0500

    Cleanup includes in SystemCClang

--- src/SystemCClang.h
+++ src/SystemCClang.h
@@ -23,33 +23,14 @@
 #include "clang/Tooling/CommonOptionsParser.h"
 #include "clang/Tooling/Tooling.h"
 
-// using namespace clang::driver;
-
-// / This is the include files we add to parse SystemC
-#include "matchers/FindArgument.h"
-#include "matchers/FindConstructor.h"
-#include "matchers/FindEntryFunctions.h"
-#include "matchers/FindEvents.h"
-#include "matchers/FindGlobalEvents.h"
-#include "matchers/FindModule.h"
-#include "matchers/FindModuleInstance.h"
-#include "matchers/FindNetlist.h"
-#include "matchers/FindNotify.h"
-#include "matchers/FindSCMain.h"
-#include "matchers/FindSimTime.h"
-#include "matchers/FindTLMInterfaces.h"
-#include "matchers/FindTemplateParameters.h"
-#include "matchers/FindWait.h"
-#include "matchers/InstanceMatcher.h"
 #include "model/Model.h"
 
 //#include "SCuitable/FindGPUMacro.h"
 //#include "SCuitable/GlobalSuspensionAutomata.h"
 //#include "SuspensionAutomata.h"
 
-using namespace clang::tooling;
-
 namespace systemc_clang {
+using namespace clang::tooling;
 /** @brief This is the main consumer class that beings the parsing of SystemC.
 
     This class drives the AST consumer for parsing SystemC constructs.
@@ -63,28 +44,28 @@ class SystemCConsumer : public ASTConsumer,
   llvm::raw_ostream &os_;
 
  public:
-  SystemCConsumer(CompilerInstance &, std::string top = "!none");
-  SystemCConsumer(ASTUnit *from_ast, std::string top = "!none");
+  SystemCConsumer(clang::CompilerInstance &, std::string top = "!none");
+  SystemCConsumer(clang::ASTUnit *from_ast, std::string top = "!none");
   virtual ~SystemCConsumer();
 
   Model *getSystemCModel();
   const std::string &getTopModule() const;
   void setTopModule(const std::string &top_module_decl);
-  ASTContext &getContext() const;
-  SourceManager &getSourceManager() const;
+  clang::ASTContext &getContext() const;
+  clang::SourceManager &getSourceManager() const;
 
   // Virtual methods that plugins may override.
   virtual bool fire();
   virtual bool preFire();
   virtual bool postFire();
 
-  virtual void HandleTranslationUnit(ASTContext &context);
+  virtual void HandleTranslationUnit(clang::ASTContext &context);
 
  private:
   std::string top_;
   Model *systemcModel_;
-  ASTContext &context_;
-  SourceManager &sm_;
+  clang::ASTContext &context_;
+  clang::SourceManager &sm_;
 };  // End class SystemCConsumer
 
 //
@@ -101,7 +82,7 @@ class SystemCConsumer : public ASTConsumer,
  */
 class SystemCClang : public SystemCConsumer {
  public:
-  SystemCClang(CompilerInstance &ci, const std::string &top)
+  SystemCClang(clang::CompilerInstance &ci, const std::string &top)
       : SystemCConsumer(ci, top) {}
 };
 
