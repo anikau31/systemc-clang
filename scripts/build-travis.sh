b2b1bd54e1dc5976d1299743aece5a301c2f9892
rmrf <nospam@uwaterloo.ca>
2021-01-08 17:27:14 -0500

    Add a build script for travis

---
+++ scripts/build-travis.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pip3 install -r $SYSTEMC_CLANG/requirements.txt
+#export SYSTEMC_CLANG=/systemc-clang
+#export SYSTEMC_CLANG_BUILD_DIR=/systemc-clang-build
+
+#mkdir $SYSTEMC_CLANG_BUILD_DIR; 
+cd $SYSTEMC_CLANG_BUILD_DIR;
+cmake ../systemc-clang -DHDL=ON -DENABLE_VERILOG_TESTS=OFF -DENABLE_TESTS=ON -G "Ninja" && ninja
+
