dcb0b326f3cd03b2468066466ab30b1714a23bc3
rmrf <nospam@uwaterloo.ca>
2021-01-19 10:14:26 -0500

    Explicitly create XML

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,6 +22,7 @@ read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 if read_the_docs_build:
 
     subprocess.call('doxygen Doxyfile', shell=True)
+    subprocess.call('make xml', shell=True)
 
 
 #  import subprocess
