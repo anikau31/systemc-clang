529bde2dd4e20c79a536b76b96c84b198da5cc79
rmrf <nospam@uwaterloo.ca>
2021-01-30 17:08:53 -0500

    Fix path for Doxygen configuration file.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
 if read_the_docs_build:
 
-    subprocess.call('doxygen Doxyfile', shell=True)
+    subprocess.call('doxygen ../../Doxyfile', shell=True)
 
 
 #  import subprocess
