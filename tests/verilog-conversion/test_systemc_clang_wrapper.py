ea960f71c5e1880cc30e277ca737369c40e847e7
Zhuanhao Wu <zhwu95@gmail.com>
2021-06-29 18:53:58 -0400

    An attempt to fix ClangArgs.h.in issue for verilog-conversion tests

--- tests/verilog-conversion/test_systemc_clang_wrapper.py
+++ tests/verilog-conversion/test_systemc_clang_wrapper.py
@@ -14,21 +14,21 @@ def syntax_error_cpp():
             )
 
 
-def test_syntax_error(tmp_path, syntax_error_cpp, default_params):
+def test_syntax_error(tmp_path, syntax_error_cpp, default_params, clang_args_params):
     target_path = tmp_path / 'simple.cpp'
     with open(target_path, 'w') as f:
         f.writelines(syntax_error_cpp)
 
     with pytest.raises(sysc_clang.SystemCClangFatalError):
-        target = sysc_clang.invoke_sysc([str(target_path), '--debug', '--'] + default_params)
+        target = sysc_clang.invoke_sysc([str(target_path), '--debug', '--'] + default_params + clang_args_params)
 
 
-def test_generate_file(tmp_path, simple_add_cpp, default_params):
+def test_generate_file(tmp_path, simple_add_cpp, default_params, clang_args_params):
     target_path = tmp_path / 'simple.cpp'
     with open(target_path, 'w') as f:
         f.writelines(simple_add_cpp)
 
-    target = sysc_clang.invoke_sysc([str(target_path), '--debug', '--'] + default_params)
+    target = sysc_clang.invoke_sysc([str(target_path), '--debug', '--'] + default_params + clang_args_params)
     assert Path(target).exists(), 'hcode file is not found'
 
 
