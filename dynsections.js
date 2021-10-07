0b2b0c0e4e08cc38378e27227651b6acfe9d195f
rseac <rseac@users.noreply.github.com>
2021-10-07 02:10:55 +0000

    deploy: 85053894a32716e50410f55343152f200368183f

--- dynsections.js
+++ dynsections.js
@@ -119,3 +119,9 @@ function toggleInherit(id)
   }
 }
 /* @license-end */
+$(document).ready(function() {
+  $('.code,.codeRef').each(function() {
+    $(this).data('powertip',$('#a'+$(this).attr('href').replace(/.*\//,'').replace(/[^a-z_A-Z0-9]/g,'_')).html());
+    $(this).powerTip({ placement: 's', smartPlacement: true, mouseOnToPopup: true });
+  });
+});
