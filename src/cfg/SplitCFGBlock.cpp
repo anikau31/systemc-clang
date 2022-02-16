ae2896958707f80fde8dd78f397e2c15faa184a5
rmrf <nospam@uwaterloo.ca>
2022-02-16 11:09:37 -0500

    SplitCFGBlock now identifies if its terminator has a wait or break statement

--- src/cfg/SplitCFGBlock.cpp
+++ src/cfg/SplitCFGBlock.cpp
@@ -7,6 +7,8 @@ using namespace systemc_clang;
 SplitCFGBlock::SplitCFGBlock()
     : block_{nullptr},
       has_wait_{false},
+      terminator_has_break_{false},
+      terminator_has_wait_{false},
       id_{0},
       next_state_{0},
       wait_arg_{32, 0, false} {}
@@ -20,7 +22,13 @@ SplitCFGBlock::SplitCFGBlock(const SplitCFGBlock& from) {
   next_state_ = from.next_state_;
   id_ = from.id_;
   wait_arg_ = from.wait_arg_;
+  terminator_has_break_ = from.terminator_has_break_;
+  terminator_has_wait_ = from.terminator_has_wait_;
 }
+
+bool SplitCFGBlock::hasTerminatorBreak() const { return terminator_has_break_; }
+
+bool SplitCFGBlock::hasTerminatorWait() const { return terminator_has_wait_; }
 
 void SplitCFGBlock::setNextState(unsigned int state) { next_state_ = state; }
 
@@ -83,6 +91,21 @@ unsigned int SplitCFGBlock::getBlockID() const { return id_; }
 unsigned int SplitCFGBlock::getNextState() const { return next_state_; }
 
 llvm::APInt SplitCFGBlock::getWaitArg() const { return wait_arg_; }
+
+void SplitCFGBlock::identifyBreaks(clang::ASTContext& context) {
+  if (block_ && block_->getTerminator().isValid()) {
+      /// See if we can iterate through all the terminator code.
+      auto stmt{ block_->getTerminator().getStmt() };
+      MatchFinder bm_reg{};
+      BreakMatcher bm{};
+      bm.registerMatchers(bm_reg);
+      bm_reg.match(*stmt, context);
+      terminator_has_wait_ = bm.hasWait();
+      terminator_has_break_ = bm.hasBreak();
+  }
+
+
+}
 
 void SplitCFGBlock::dump() const {
   if (block_) {
