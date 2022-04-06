a0d5f6696a859d538e9802e4877fe2c1d99470e8
rmrf <nospam@uwaterloo.ca>
2022-04-06 10:29:30 -0400

    Continue cleaning up includes

--- src/matchers/FindEvents.h
+++ src/matchers/FindEvents.h
@@ -1,33 +1,34 @@
 #ifndef _FIND_EVENTS_H_
 #define _FIND_EVENTS_H_
 
-#include "clang/AST/DeclCXX.h"
 #include "clang/AST/RecursiveASTVisitor.h"
 #include "llvm/Support/raw_ostream.h"
 #include <map>
 #include <string>
 #include <vector>
-//#include "ReflectionContainerClass.h"
+
+namespace clang {
+  class FieldDecl;
+  class CXXRecordDecl;
+};
 
 namespace systemc_clang {
-using namespace clang;
-using namespace std;
 
-class FindEvents : public RecursiveASTVisitor<FindEvents> {
+class FindEvents : public clang::RecursiveASTVisitor<FindEvents> {
 public:
   /// typedefs
-  typedef map<string, FieldDecl *> classEventMapType;
-  typedef pair<string, FieldDecl *> kvType;
+  typedef std::map<std::string, clang::FieldDecl *> classEventMapType;
+  typedef std::pair<std::string, clang::FieldDecl *> kvType;
 
-  FindEvents(CXXRecordDecl *, llvm::raw_ostream &);
+  FindEvents(clang::CXXRecordDecl *, llvm::raw_ostream &);
   FindEvents(const FindEvents &);
   virtual ~FindEvents();
   /// RecursiveASTVisitor methods
-  virtual bool VisitFieldDecl(FieldDecl *);
+  virtual bool VisitFieldDecl(clang::FieldDecl *);
 
   /// Access methods
   classEventMapType getInClassEvents();
-  vector<string> getEventNames();
+  std::vector<std::string> getEventNames();
 
   /// Print methods
   void dump();
