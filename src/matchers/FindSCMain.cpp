005840b9bdd4b3205fd3fe9c39fbd2a7112f7194
rmrf <nospam@uwaterloo.ca>
2022-04-06 10:36:55 -0400

    Continue cleaning up includes

--- src/matchers/FindSCMain.cpp
+++ src/matchers/FindSCMain.cpp
@@ -1,8 +1,11 @@
 #include "FindSCMain.h"
 
+#include "clang/AST/DeclCXX.h"
+
 using namespace systemc_clang;
 
-FindSCMain::FindSCMain(TranslationUnitDecl *tuDecl, llvm::raw_ostream &os)
+FindSCMain::FindSCMain(clang::TranslationUnitDecl *tuDecl,
+                       llvm::raw_ostream &os)
     : os_{os}, sc_main_function_declaration_{nullptr} {
   assert(!(tuDecl == nullptr));
   TraverseDecl(tuDecl);
@@ -10,7 +13,7 @@ FindSCMain::FindSCMain(TranslationUnitDecl *tuDecl, llvm::raw_ostream &os)
 
 FindSCMain::~FindSCMain() { sc_main_function_declaration_ = nullptr; }
 
-bool FindSCMain::VisitFunctionDecl(FunctionDecl *function_declaration) {
+bool FindSCMain::VisitFunctionDecl(clang::FunctionDecl *function_declaration) {
   /// Find sc_main.
   /// There are three conditions to satisfy this:
   /// 1. Must have sc_main in its name.
@@ -26,7 +29,7 @@ bool FindSCMain::VisitFunctionDecl(FunctionDecl *function_declaration) {
   return true;
 }
 
-FunctionDecl *FindSCMain::getSCMainFunctionDecl() const {
+clang::FunctionDecl *FindSCMain::getSCMainFunctionDecl() const {
   assert(sc_main_function_declaration_ != nullptr);
 
   return sc_main_function_declaration_;
