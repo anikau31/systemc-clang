7b2fb7395691c88d04532b9f1bfec40c54aa85ac
rseac <rseac@users.noreply.github.com>
2022-04-07 13:44:07 +0000

    deploy: fe3f8f58d127191baccd69ce6138725488525e6a

--- search/typedefs_b.js
+++ search/typedefs_b.js
@@ -6,7 +6,7 @@ var searchData=
   ['portmaptype_3',['portMapType',['../classsystemc__clang_1_1ModuleInstance.html#ae5e4d7d956225b28aac30182a5649f84',1,'systemc_clang::ModuleInstance']]],
   ['portsignalmaptype_4',['portSignalMapType',['../classsystemc__clang_1_1FindNetlist.html#a9f9a1a9b49e67f0d8861887ccbdabc01',1,'systemc_clang::FindNetlist::portSignalMapType()'],['../classsystemc__clang_1_1ModuleInstance.html#ab9826602b77bf4d1b56a2f9dac9fff52',1,'systemc_clang::ModuleInstance::portSignalMapType()']]],
   ['portsignalpairtype_5',['portSignalPairType',['../classsystemc__clang_1_1FindNetlist.html#a149fe83ade7e3468bc49ae42dee28a46',1,'systemc_clang::FindNetlist::portSignalPairType()'],['../classsystemc__clang_1_1ModuleInstance.html#a225eb7d03ade18036aa5700538476541',1,'systemc_clang::ModuleInstance::portSignalPairType()']]],
-  ['porttype_6',['PortType',['../classsystemc__clang_1_1FindPorts.html#a1989d200f0df2b414b1b5a57259533e3',1,'systemc_clang::FindPorts::PortType()'],['../classsystemc__clang_1_1ModuleInstance.html#a2c28ab5fe658b9833300e3d979912f42',1,'systemc_clang::ModuleInstance::PortType()']]],
+  ['porttype_6',['PortType',['../classsystemc__clang_1_1ModuleInstance.html#a2c28ab5fe658b9833300e3d979912f42',1,'systemc_clang::ModuleInstance']]],
   ['processmaptype_7',['processMapType',['../classsystemc__clang_1_1ModuleInstance.html#aa971eba18c75ee5a38fcfc9739bbf9a5',1,'systemc_clang::ModuleInstance']]],
   ['processpairtype_8',['processPairType',['../classsystemc__clang_1_1ModuleInstance.html#a4674bdaba29e097bef92dccd45888fdd',1,'systemc_clang::ModuleInstance']]],
   ['processwaiteventmaptype_9',['processWaitEventMapType',['../classsystemc__clang_1_1FindWait.html#aace39df59ecd7e78771ebc29d6a606c5',1,'systemc_clang::FindWait']]],
