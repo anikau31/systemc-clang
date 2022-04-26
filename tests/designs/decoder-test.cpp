db3075f405e12ab20786d29544c8ff83ded7e89e
rmrf <nospam@uwaterloo.ca>
2022-04-25 20:07:17 -0400

    Update decoder test

--- tests/designs/decoder-test.cpp
+++ tests/designs/decoder-test.cpp
@@ -9,7 +9,7 @@ using namespace systemc_clang;
 
 TEST_CASE("Read SystemC model from file for testing") {
   std::string code{systemc_clang::read_systemc_file(
-      systemc_clang::test_data_dir, "designs/decoder-input.cpp")};
+      systemc_clang::test_data_dir, "decoder-input.cpp")};
 
   ASTUnit *from_ast =
       tooling::buildASTFromCodeWithArgs(code, systemc_clang::catch_test_args)
