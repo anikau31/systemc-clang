a1ac9ccfb2f8c74397573ca9ad64e3c4a51924ff
rmrf <nospam@uwaterloo.ca>
2022-09-06 05:58:10 -0400

    Use and test Visit*

--- plugins/hdl/HDLBody.h
+++ plugins/hdl/HDLBody.h
@@ -42,27 +42,28 @@ class HDLBody : public RecursiveASTVisitor<HDLBody> {
   void Run(Stmt *stmt, hNodep &h_top, HDLBodyMode runmode,
            HDLType *HDLt_userclassesp = NULL);
 
+  bool TraverseCXXOperatorCallExpr(CXXOperatorCallExpr *opcall);
+  bool TraverseCXXMemberCallExpr(CXXMemberCallExpr *callexpr);
+  bool TraverseCallExpr(CallExpr *callexpr);
+
   bool TraverseCompoundStmt(CompoundStmt *compoundStmt);
   bool TraverseStmt(Stmt *stmt);
   bool TraverseDeclStmt(DeclStmt *declstmt);
   bool ProcessVarDecl(VarDecl *vardecl);
-  bool TraverseBinaryOperator(BinaryOperator *expr);
-  bool TraverseUnaryOperator(UnaryOperator *expr);
-  bool TraverseConditionalOperator(ConditionalOperator *expr);
-  bool TraverseIntegerLiteral(IntegerLiteral *lit);
-  bool TraverseCXXBoolLiteralExpr(CXXBoolLiteralExpr *b);
+  bool VisitBinaryOperator(BinaryOperator *expr);
+  bool VisitUnaryOperator(UnaryOperator *expr);
+  bool VisitConditionalOperator(ConditionalOperator *expr);
+  bool VisitIntegerLiteral(IntegerLiteral *lit);
+  bool VisitCXXBoolLiteralExpr(CXXBoolLiteralExpr *b);
   bool TraverseDeclRefExpr(DeclRefExpr *expr);
   bool TraverseArraySubscriptExpr(ArraySubscriptExpr *expr);
-  bool TraverseCXXMemberCallExpr(CXXMemberCallExpr *callexpr);
-  bool TraverseCXXOperatorCallExpr(CXXOperatorCallExpr *opcall);
-  bool TraverseCallExpr(CallExpr *callexpr);
   bool TraverseMemberExpr(MemberExpr *memberexpr);
-  bool TraverseIfStmt(IfStmt *ifs);
-  bool TraverseForStmt(ForStmt *fors);
-  bool TraverseSwitchStmt(SwitchStmt *switchs);
+  bool VisitIfStmt(IfStmt *ifs);
+  bool VisitForStmt(ForStmt *fors);
+  bool VisitSwitchStmt(SwitchStmt *switchs);
   bool ProcessSwitchCase(SwitchCase *cases);
-  bool TraverseWhileStmt(WhileStmt *whiles);
-  bool TraverseDoStmt(DoStmt *whiles);
+  bool VisitWhileStmt(WhileStmt *whiles);
+  bool VisitDoStmt(DoStmt *whiles);
   string FindVname(NamedDecl *vard);
   void AddVnames(hNodep &hvns);
   string FindFname(FunctionDecl *funcd);
