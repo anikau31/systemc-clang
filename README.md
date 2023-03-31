d5848ef25a51e77cfb8fda706235e12ebfbaae92
Zhuanhao Wu <zhwu95@gmail.com>
2023-03-31 04:18:42 -0400

    Update

--- README.md
+++ README.md
@@ -36,16 +36,75 @@ docker --version
 docker build -t fccm-ae
 ```
 
-- Run the docker container in interactive mode
+- After building the docker image, go to the folder containing the systemc-clang, start the docker image with start.sh with the following command
 ```
-docker run -it fccm-ae
+cd ..
+mkdir systemc-clang-build
+./systemc-clang/start.sh fccm-ae fccm-ae-dev systemc-clang systemc-clang-build
 ```
 
-- Once you are inside the container, you can execute all the case studies through SCCL by invoking the `step1.sh`
+- The following steps should be conducted in the container.
 
+- Build systemc-clang within the docker image under `/systemc-clang-build` using `cmake` and `ninja`.
+```
+cmake ../systemc-clang -DHDL=ON -DENABLE_TESTS=ON -DENABLE_VERILOG_TESTS=ON -DCMAKE_BUILD_TYPE=Debug -DENABLE_HARDWARE_FLOW=ON -G Ninja
+```
+
+- Within the systemc-clang source directory, execute `step1.sh`. 
+This command automatically runs systemc-clang and generates the IR and the SystemVerilog design files.
 ```
 cd /systemc-clang/;
 sh step1.sh;
 ```
 
-You will notice that the generated Verilog files will be within the respective directories for the case studies.
+- Leave the docker container and the generated files could be found in various directories in the SCCL folders, specifically in `systemc-clang/fccm-case-studies`.
+
+- Go to the bitstream generation folder on the host, and modify `env.sh`, such that `ULTRA96_V2_SSH_NAME` points to the Ultra96V2 host name and `ULTRA96_V2_PLATFORM` points to the official Ultra96V2 platform file:
+```
+cd systemc-clang/fccm-case-studies/ultra96-vitis-ae/
+cat env.sh
+
+export ULTRA96_V2_PLATFORM=/opt/vitis_platforms/u96v2_sbc_base/u96v2_sbc_base.xpfm
+export ULTRA96_V2_SSH_NAME=ultra96v2
+```
+
+- Next, source the env.sh
+```
+source env.sh
+```
+
+- Ensure that Vivado is 2022.1
+```
+vivado --version
+
+Vivado v2022.1 (64-bit)
+SW Build 3526262 on Mon Apr 18 15:47:01 MDT 2022
+IP Build 3524634 on Mon Apr 18 20:55:01 MDT 2022
+Tool Version Limit: 2022.04
+
+v++ --version
+
+****** v++ v2022.1 (64-bit)
+  **** SW Build 3524075 on 2022-04-13-17:42:45
+    ** Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+```
+
+- Build the bitstreams/utilization
+```
+make bitstreams
+make utils
+```
+
+- To build the bitstreams for the zhw encoder/decoder, go to the parent folder of the repository, and create another directory.
+```
+mkdir systemc-clang-build-zfp && cd systemc-clang-build-zfp
+cmake ../systemc-clang -DHDL=ON -DENABLE_HARDWARE_FLOW=ON -DClang_DIR=/opt/clang-13/
+make DS_top_z7_d64_bitstream DS_top_z7_d32_bitstream DS_top_z3_d64_bitstream DS_top_z3_d32_bitstream
+```
+
+- Go back to `ultra96-vitis-ae` folder and upload the bitstreams
+```
+cd systemc-clang/fccm-case-studies/ultra96-vitis-ae/
+bash upload.sh
+```
+- We provide a notebook file for running the bitstreams at `systemc-clang/fccm-case-studies/ultra96-vitis-ae/FCCM-2023-AE.ipynb`
--- step1.sh
+++ step1.sh
@@ -10,46 +10,46 @@ cd $SRC;
 
 # The following commands will generate SystemVerilog directly
 
-# # A. MA
-# $RUN ultra96-vitis-ae/moving-average-d16-w16/moving-average.cpp -I$SRC/moving-average -I$SRC/zfpsynth
-# $RUN ultra96-vitis-ae/moving-average-d16-w64/moving-average.cpp -I$SRC/moving-average -I$SRC/zfpsynth
-# $RUN ultra96-vitis-ae/moving-average-d64-w16/moving-average.cpp -I$SRC/moving-average -I$SRC/zfpsynth
-# $RUN ultra96-vitis-ae/moving-average-d64-w64/moving-average.cpp -I$SRC/moving-average -I$SRC/zfpsynth
-# 
-# Shift
-# $RUN ultra96-vitis-ae/moving-average-shift-d16-w16/moving-average.cpp -I$SRC/moving-average-shift -I$SRC/zfpsynth
-# $RUN ultra96-vitis-ae/moving-average-shift-d16-w64/moving-average.cpp -I$SRC/moving-average-shift -I$SRC/zfpsynth
-# $RUN ultra96-vitis-ae/moving-average-shift-d64-w16/moving-average.cpp -I$SRC/moving-average-shift -I$SRC/zfpsynth
-# $RUN ultra96-vitis-ae/moving-average-shift-d64-w64/moving-average.cpp -I$SRC/moving-average-shift -I$SRC/zfpsynth
+# A. MA
+$RUN ultra96-vitis-ae/moving-average-d16-w16/moving-average.cpp -I$SRC/moving-average -I$SRC/zfpsynth
+$RUN ultra96-vitis-ae/moving-average-d16-w64/moving-average.cpp -I$SRC/moving-average -I$SRC/zfpsynth
+$RUN ultra96-vitis-ae/moving-average-d64-w16/moving-average.cpp -I$SRC/moving-average -I$SRC/zfpsynth
+$RUN ultra96-vitis-ae/moving-average-d64-w64/moving-average.cpp -I$SRC/moving-average -I$SRC/zfpsynth
 
-# # pipe
-# $RUN ultra96-vitis-ae/moving-average-pipe-d16-w16/moving-average.cpp -I$SRC/moving-average-pipe -I$SRC/zfpsynth
-# $RUN ultra96-vitis-ae/moving-average-pipe-d16-w64/moving-average.cpp -I$SRC/moving-average-pipe -I$SRC/zfpsynth
-# $RUN ultra96-vitis-ae/moving-average-pipe-d64-w16/moving-average.cpp -I$SRC/moving-average-pipe -I$SRC/zfpsynth
-# $RUN ultra96-vitis-ae/moving-average-pipe-d64-w64/moving-average.cpp -I$SRC/moving-average-pipe -I$SRC/zfpsynth
-# 
+Shift
+$RUN ultra96-vitis-ae/moving-average-shift-d16-w16/moving-average.cpp -I$SRC/moving-average-shift -I$SRC/zfpsynth
+$RUN ultra96-vitis-ae/moving-average-shift-d16-w64/moving-average.cpp -I$SRC/moving-average-shift -I$SRC/zfpsynth
+$RUN ultra96-vitis-ae/moving-average-shift-d64-w16/moving-average.cpp -I$SRC/moving-average-shift -I$SRC/zfpsynth
+$RUN ultra96-vitis-ae/moving-average-shift-d64-w64/moving-average.cpp -I$SRC/moving-average-shift -I$SRC/zfpsynth
+
+# pipe
+$RUN ultra96-vitis-ae/moving-average-pipe-d16-w16/moving-average.cpp -I$SRC/moving-average-pipe -I$SRC/zfpsynth
+$RUN ultra96-vitis-ae/moving-average-pipe-d16-w64/moving-average.cpp -I$SRC/moving-average-pipe -I$SRC/zfpsynth
+$RUN ultra96-vitis-ae/moving-average-pipe-d64-w16/moving-average.cpp -I$SRC/moving-average-pipe -I$SRC/zfpsynth
+$RUN ultra96-vitis-ae/moving-average-pipe-d64-w64/moving-average.cpp -I$SRC/moving-average-pipe -I$SRC/zfpsynth
+
 # B.ZFP enc/dec
 # E1
 $RUN zfpsynth/zfp3/z3test.cpp
 cp $SRC/zfpsynth/zfp3/z3test_hdl.txt{.v,.sv}
-# # E2
-# $RUN zfpsynth/zfp3-fp32/z3test.cpp
-# cp $SRC/zfpsynth/zfp3-fp32/z3test_hdl.txt{.v,.sv}
-# # D1
-# $RUN zfpsynth/zfp7/z7test.cpp
-# cp $SRC/zfpsynth/zfp7/z7test_hdl.txt{.v,.sv}
-# # D2
-# $RUN zfpsynth/zfp7_fp32_2d/z7test.cpp
-# cp $SRC/zfpsynth/zfp7_fp32_2d/z7test_hdl.txt{.v,.sv}
-# 
-# # C.systolic array
-# $RUN ultra96-vitis-ae/mm-n8-d8/mm.cpp
-# $RUN ultra96-vitis-ae/mm-n16-d8/mm.cpp
-# 
-# # D.Threads with div
-# $RUN ultra96-vitis-ae/div-8/divider.cpp -I$SRC/divider-thread -I$SRC/zfpsynth
-# $RUN ultra96-vitis-ae/div-16/divider.cpp -I$SRC/divider-thread -I$SRC/zfpsynth
-# $RUN ultra96-vitis-ae/div-32/divider.cpp -I$SRC/divider-thread -I$SRC/zfpsynth
-# $RUN ultra96-vitis-ae/div-64/divider.cpp -I$SRC/divider-thread -I$SRC/zfpsynth
+# E2
+$RUN zfpsynth/zfp3-fp32/z3test.cpp
+cp $SRC/zfpsynth/zfp3-fp32/z3test_hdl.txt{.v,.sv}
+# D1
+$RUN zfpsynth/zfp7/z7test.cpp
+cp $SRC/zfpsynth/zfp7/z7test_hdl.txt{.v,.sv}
+# D2
+$RUN zfpsynth/zfp7_fp32_2d/z7test.cpp
+cp $SRC/zfpsynth/zfp7_fp32_2d/z7test_hdl.txt{.v,.sv}
 
-# # Rename from sv -> v
+# C.systolic array
+$RUN ultra96-vitis-ae/mm-n8-d8/mm.cpp
+$RUN ultra96-vitis-ae/mm-n16-d8/mm.cpp
+
+# D.Threads with div
+$RUN ultra96-vitis-ae/div-8/divider.cpp -I$SRC/divider-thread -I$SRC/zfpsynth
+$RUN ultra96-vitis-ae/div-16/divider.cpp -I$SRC/divider-thread -I$SRC/zfpsynth
+$RUN ultra96-vitis-ae/div-32/divider.cpp -I$SRC/divider-thread -I$SRC/zfpsynth
+$RUN ultra96-vitis-ae/div-64/divider.cpp -I$SRC/divider-thread -I$SRC/zfpsynth
+
+# Rename from sv -> v
